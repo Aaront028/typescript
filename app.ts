5e711404d8026ac2ca169a6a8016d1b005564714
Aaron Tan <info@aarontan.co.nz>
2022-10-12 02:43:37 +1300

    learning arrays and tuples

--- app.ts
+++ app.ts
@@ -1,9 +1,32 @@
+// const person: {
+//   name: string;
+//   age: number;
+// }
+
 const person: {
   name: string;
   age: number;
+  hobbies: string[];
+  role: [number, string];
 } = {
   name: "Aaron",
-  age: 30
+  age: 30,
+  hobbies: ['Music', 'Cooking', 'Planting'],
+  role: [2, 'author']
 }
 
-console.log(person)
+// person.role.push('admin')
+// person.role[1] = 10 // this is allowed
+
+person.role = [0, 'admin', 'user'];
+
+let favouriteActivities: string[];
+// ------ below gives you the benefits of anytype (back into javascript world)
+// let favouriteActivities: any[]; 
+favouriteActivities = ['Sports']
+
+console.log(person.role)
+
+for (const hobby of person.hobbies) {
+  console.log(hobby);
+}
